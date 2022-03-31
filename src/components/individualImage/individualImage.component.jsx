d0071cf58fe5b1d38ee5faf28a9f348b5ca33c33
Julien Fleury <julien@fleuryphotography.com>
2022-03-31 19:17:18 +0200

    Added more input fields as well as style changes. Need to update metadata on initial input and add some placeholders.

--- src/components/individualImage/individualImage.component.jsx
+++ src/components/individualImage/individualImage.component.jsx
@@ -7,8 +7,13 @@ const IndividualImage = ({ data, index }) => {
     // console.log(data.images[index]);
     const pickedImage = data.images[index];
 
+    // Add placeholders! Do Use Effect. Change metadata.
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [creationDate, setCreationDate] = useState('Fevrier 2022');
+    const [height, setHeight] = useState('100 cm');
+    const [width, setWidth] = useState('50 cm');
+
     useEffect(() => {
         setTitle(pickedImage.title);
         setDescription(pickedImage.description);
@@ -24,6 +29,15 @@ const IndividualImage = ({ data, index }) => {
             case 'description':
                 setDescription(value);
                 break;
+            case 'creationDate':
+                setCreationDate(value);
+                break;
+            case 'height':
+                setHeight(value);
+                break;
+            case 'width':
+                setWidth(value);
+                break;
             default:
                 break;
         }
@@ -36,7 +50,21 @@ const IndividualImage = ({ data, index }) => {
             <div className="individualImageInfoContainer">
                 <h4>{title}</h4>
                 <div className="individualImageInput">
+                    <label className="inputLabel" htmlFor="title">Titre</label>
                     <CustomInput id={'title'} type={'text'} name={'title'} value={title} onChange={onInputChange} />
+                    <label className="inputLabel" htmlFor="creationDate">Date de Creation</label>
+                    <CustomInput id={'creationDate'} type={'text'} name={'creationDate'} value={creationDate} onChange={onInputChange} />
+                    <div className="dimensionContainer">
+                        <div className="heightBox">
+                            <label className="inputLabel" htmlFor="height">Hauteur</label>
+                            <CustomInput id={'height'} type={'text'} name={'height'} value={height} onChange={onInputChange} />
+                        </div>
+                        <div className="widthBox ms-auto">
+                            <label className="inputLabel" htmlFor="height">Largeur</label>
+                            <CustomInput id={'width'} type={'text'} name={'width'} value={width} onChange={onInputChange} />
+                        </div>
+                    </div>
+                    <label className="inputLabel" htmlFor="description">Description</label>
                     <CustomTextBox id={'description'} name={'description'} value={description} onChange={onInputChange} />
                 </div>
 
