5c96066323452f9fb7f5c543621ea99b90808ae5
Julien Fleury <julien@fleuryphotography.com>
2022-04-15 16:48:17 +0200

    Added FR EN switch to editTextsection.

--- src/components/editTextSection/editTextSection.component.jsx
+++ src/components/editTextSection/editTextSection.component.jsx
@@ -6,6 +6,8 @@ import TextEditModule from "../textEditModule/textEditModule.component";
 
 
 const EditTextSection = ({ textData }) => {
+    // LangClick
+    const [language, setLanguage] = useState('FR');
 
     const [aboutInfo, setAboutInfo] = useState('This Information Needs To Be Updated');
     const [homeInfo, setHomeInfo] = useState('This Information Needs To Be Updated Too');
@@ -31,9 +33,20 @@ const EditTextSection = ({ textData }) => {
         setHomeInfo(textData.homeInfo);
     }, []);
 
+    const onLangClick = (lang) => {
+        setLanguage(lang);
+    }
+
     return (
         <div className="editTextSectionContainer">
+            <div className="headerSection">
             <h1 className="mb-3">Edit Text Section</h1>
+                <div className="langSwitch">
+                    <h6 onClick={() => onLangClick('FR')} className={`${(language === 'FR') ? 'activeLang' : ''} frLang`}>FR</h6>
+                    <div className="">-</div>
+                    <h6 onClick={() => onLangClick('EN')} className={`${(language === 'EN') ? 'activeLang' : ''} enLang`}>EN</h6>
+                </div>
+            </div>
             <div className="moduleContainer mb-5">
                 <TextEditModule id={'aboutInfo'} name={'aboutInfo'} value={aboutInfo} placeholder={'About Information'} displayName={'About Info'} onChangeHandle={onChangeHandle} />
             </div>
